8e11cfd2b1492d636257b223623a97e48c01994a
masato <tsujii26@up.edu>
2023-02-02 18:15:30 -0800

    Lab 2 checkpoint 4

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -1,4 +1,7 @@
 package cs301.birthdaycake;
+import android.view.View;
+import android.util.Log;
+
 
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
@@ -11,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+    }
+    public void goodbye(View button) {
+        Log.i("button", "Goodbye");
     }
 }

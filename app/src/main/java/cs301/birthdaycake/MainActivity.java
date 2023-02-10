1736ad78136828b35bba049705ddc581718e403a
masato <tsujii26@up.edu>
2023-02-09 18:56:12 -0800

    Lab 3 checkpoint 2

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         CakeView view = findViewById(R.id.cakeview);
         CakeController controller = new CakeController(view);
+        Button blowOutButton = findViewById(R.id.button);
+        blowOutButton.setOnClickListener(controller);
     }
     public void goodbye(View button) {
         Log.i("button", "Goodbye");

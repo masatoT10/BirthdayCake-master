f5e2aeee00f77d61f393770e0cae4033f93ca4af
masato <tsujii26@up.edu>
2023-02-23 17:26:00 -0800

    Lab 4 checkpoint 3  Masato

--- app/src/main/java/cs301/birthdaycake/CakeModel.java
+++ app/src/main/java/cs301/birthdaycake/CakeModel.java
@@ -5,14 +5,10 @@ public class CakeModel {
     public float touchY;
 
     boolean candleLit = true;
-     public int numCandle = 2;
+    public int numCandle = 2;
+
     private boolean frosting = true;
     boolean hasCandle = true;
-    public float touchX;
-    public float touchY;
-
-    public float h = 200.f;
-    public float w = 130.0f;
 
     boolean hasTouched = false;
 
